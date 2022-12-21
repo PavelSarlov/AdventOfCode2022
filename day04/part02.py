59a7bb55fa9e092c7c81e4b8ba5e2a458f6134f0
Pavel Sarlov <sarlovpavel@gmail.com>
2022-12-21 17:50:33 +0200

    add day 4

--- day04/part02.py
+++ day04/part02.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
         for line in lines:
             [e1, e2] = [[int(bound) for bound in elf.split('-')] for elf in line.strip().split(',')]
 
-            if (e1[0] <= e2[0] and e1[1] >= e2[1]) or (e1[0] >= e2[0] and e1[1] <= e2[1]):
+            if (e1[0] <= e2[1] and e1[1] >= e2[0]) or (e1[0] >= e2[1] and e1[1] <= e2[0]):
                 count = count + 1
                 
         print(count)
